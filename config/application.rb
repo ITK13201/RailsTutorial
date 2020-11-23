0b48b74475a5f3f4e01f5e05346408795ba67383
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2020-11-24 01:29:12 +0900

    Add .editconfig

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Snchat
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.read_encrypted_secrets = true
   end
 end
