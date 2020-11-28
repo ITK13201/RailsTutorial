26dceaa80a9adbe728f4e18d5b2f9b7e3be6558f
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2020-11-29 01:11:04 +0900

    generate home controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :load_user # <= since rails 5, called first without setting
+  # before_action :load_user # <= since rails 5, called first without setting
   protect_from_forgery with: :exception # <= until rails 4, called first
 end
